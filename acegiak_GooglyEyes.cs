73c8cddf8fa3a109d8392d95ba54751f5fab4bb2
Ashton McAllan <acegiak@gmail.com>
2019-06-18 18:00:01 +0930

    First Implementation

---
+++ acegiak_GooglyEyes.cs
@@ -0,0 +1,78 @@
+using System;
+using System.Collections.Generic;
+using XRL.Rules;
+using XRL.UI;
+
+namespace XRL.World.Parts
+{
+	[Serializable]
+	public class acegiak_GooglyEyes : IPart
+	{
+		public acegiak_GooglyEyes()
+		{
+			base.Name = "Crayons";
+		}
+
+		public override bool SameAs(IPart p)
+		{
+			return false;
+		}
+
+		public override void Register(GameObject Object)
+		{
+			Object.RegisterPartEvent(this, "GetInventoryActions");
+			Object.RegisterPartEvent(this, "InvCommandGooglify");
+			base.Register(Object);
+		}
+
+
+		public void Googlify(GameObject GO){
+            GO.AddPart(new acegiak_ModGoogly());
+            Popup.Show(GO.The+GO.DisplayNameOnly+GO.GetVerb("look")+" very intimidating.");
+            ParentObject.ForceUnequipAndRemove(true);
+        }
+
+		public override bool FireEvent(Event E)
+		{
+			if (E.ID == "GetInventoryActions")
+			{
+				EventParameterGetInventoryActions eventParameterGetInventoryActions = E.GetParameter("Actions") as EventParameterGetInventoryActions;
+				eventParameterGetInventoryActions.AddAction("Googlify", 'G',  false, "&WG&yooglify", "InvCommandGooglify");
+				return true;
+			}
+			if (E.ID == "InvCommandGooglify")
+			{
+					string text2 = XRL.UI.PickDirection.ShowPicker();
+					if (text2 == null)
+					{
+						return true;
+					}
+					Cell cell = null;
+					if (ParentObject.pPhysics.Equipped != null)
+					{
+						cell = ParentObject.pPhysics.Equipped.pPhysics.CurrentCell;
+					}
+					if (ParentObject.pPhysics.InInventory != null)
+					{
+						cell = ParentObject.pPhysics.InInventory.pPhysics.CurrentCell;
+					}
+					if (ParentObject.pPhysics.CurrentCell != null)
+					{
+						cell = ParentObject.pPhysics.CurrentCell;
+					}
+					cell = cell.GetCellFromDirection(text2);
+					if (cell != null)
+					{
+							if (cell.Objects.Count > 0 && cell.Objects[0].pRender != null)
+							{
+								Googlify(cell.Objects[0]);
+							}else{
+                                Popup.Show("The googly eyes don't stick.");
+                                return false;
+                            };
+					}
+			}
+			return base.FireEvent(E);
+		}
+	}
+}
---
+++ acegiak_ModGoogly.cs
@@ -0,0 +1,94 @@
+using System;
+using System.Text;
+using XRL.Rules;
+using XRL.World.Parts.Skill;
+using XRL.World.Parts.Effects;
+using XRL.UI;
+
+namespace XRL.World.Parts
+{
+	[Serializable]
+	public class acegiak_ModGoogly : IModification
+	{
+		public int Chance = 2;
+
+		public acegiak_ModGoogly()
+		{
+		}
+
+		public acegiak_ModGoogly(int Tier)
+			: base(Tier)
+		{
+		}
+
+		public override void Configure()
+		{
+			WorksOnSelf = true;
+		}
+
+		public override bool ModificationApplicable(GameObject Object)
+		{
+			return true;
+		}
+
+		public override void ApplyModification(GameObject Object)
+		{
+			IncreaseDifficultyAndComplexityIfComplex(1, 1);
+		}
+
+		public override bool AllowStaticRegistration()
+		{
+			return true;
+		}
+
+		public override bool SameAs(IPart p)
+		{
+			return base.SameAs(p);
+		}
+
+		public override void Register(GameObject Object)
+		{
+			Object.RegisterPartEvent(this, "GetDisplayName");
+			Object.RegisterPartEvent(this, "GetShortDisplayName");
+			Object.RegisterPartEvent(this, "GetShortDescription");
+			Object.RegisterPartEvent(this, "WeaponHit");
+			Object.RegisterPartEvent(this, "AfterLookedAt");
+			base.Register(Object);
+		}
+
+		public override bool FireEvent(Event E)
+		{
+            if (E.ID == "AfterLookedAt")
+			{
+				GameObject gameObjectParameter = E.GetGameObjectParameter("Looker");
+				if (gameObjectParameter != null && !gameObjectParameter.HasEffect("Shaken"))
+				{
+                    if(gameObjectParameter.IsPlayer()){
+                        Popup.Show(gameObjectParameter.It+gameObjectParameter.Is+" disturbed by "+ParentObject.the+ParentObject.DisplayNameOnly+"'s intimidating eyes.");
+                    }else{
+                        IPart.AddPlayerMessage(gameObjectParameter.The+gameObjectParameter.DisplayNameOnly+gameObjectParameter.Is+" disturbed by "+ParentObject.the+ParentObject.DisplayNameOnly+"'s intimidating eyes.");
+                    }
+					gameObjectParameter.ApplyEffect(new Shaken(Stat.Random(300, 360), 1));
+				}
+			}
+			if (E.ID == "WeaponHit")
+			{
+				if ((Chance >= 100 || Stat.Random(1, 100) <= Chance) && IsReady(UseCharge: true))
+				{
+					GameObject gameObjectParameter = E.GetGameObjectParameter("Attacker");
+					GameObject gameObjectParameter2 = E.GetGameObjectParameter("Defender");
+                    IPart.AddPlayerMessage(gameObjectParameter2.The+gameObjectParameter2.DisplayNameOnly+gameObjectParameter2.GetVerb("is")+" disturbed by "+ParentObject.the+ParentObject.DisplayNameOnly+"'s intimidating eyes.");
+					gameObjectParameter2.ApplyEffect(new Shaken(Stat.Random(300, 360), 1));				}
+			}
+			else if (E.ID == "GetShortDescription")
+			{
+				E.AddParameter("Postfix", E.GetStringParameter("Postfix") + "\n&yg&Ko&yo&Kg&yly: This item has been adorned with an intimidating set of artificial eyes.");
+			}
+			else if ((E.ID == "GetDisplayName" || E.ID == "GetShortDisplayName") && ParentObject.Understood() && !ParentObject.HasProperName)
+			{
+				E.GetParameter<StringBuilder>("Prefix").Append("&yg&Ko&yo&Kg&yly &y");
+			}
+			return base.FireEvent(E);
+		}
+	}
+}
